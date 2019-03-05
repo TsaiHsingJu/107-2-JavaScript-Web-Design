83949b76feef04c375c767c8dca0e49808a08c6c
tsaihsingju <881006amy@gms.tku.edu.tw>
2019-03-05 22:01:57 +0800

    w03-5

--- w03-14_switch/app.js
+++ w03-14_switch/app.js
@@ -1,9 +1,23 @@
 const color = 'yellow';
 // switch whether color is red or blue or neigher
 console.log("const color = 'yellow'")
-console.log()
+console.log("switch(color) result :")
+
+switch(color)
+{
+    case 'red':
+        console.log('Color is red');
+    break;
+    case 'blue':
+        console.log('Color is blue');
+    break;
+    default;
+        console.log('Color is red or blue');
+    break;
+}
 
 let day;
+
 
 //switch newDate().getDay() and console Today's day
 
