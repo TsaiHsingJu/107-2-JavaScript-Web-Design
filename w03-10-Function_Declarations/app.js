b6de2aa4bd6acbc9cc689f68a84636b9e4aed11a
tsaihsingju <881006amy@gms.tku.edu.tw>
2019-03-05 19:15:39 +0800

    w03-2

---
+++ w03-10-Function_Declarations/app.js
@@ -0,0 +1,16 @@
+// FUNCTION DECLARATIONS
+function greet(firstName='John',lastName='Doe')
+{
+    return 'Hello' + ' ' + firstName + ' ' + lastName;
+}
+
+console.log(greet(''));
+console.log(greet('HsingJu','Tsai'));
+
+// FUNCTION EXPRESIONS
+
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+
+// PROPERTY METHODS
