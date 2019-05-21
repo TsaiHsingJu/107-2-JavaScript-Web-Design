f2215bef574cc268296d180ccbb32a88d1f5c7a8
tsaihsingju <881006amy@gms.tku.edu.tw>
2019-05-21 21:39:23 +0800

    w1-34

--- w14-HW/4/app.js
+++ w14-HW/4/app.js
@@ -57,8 +57,7 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     const ui = new UI();
     if (title1 === '' || title2 === '') {
         ui.showAlert('Please fill in all fields', 'error');
-    }
-    else {
+    } else {
         ui.addBookToList(book);
         ui.showAlert('BMI Added!', 'success');
         ui.clearFields();
@@ -76,67 +75,60 @@ document.getElementById('book-list').addEventListener('click', function (e) {
 
 ////////////////////////////////////////////////////////////////////////////////////
 // document.getElementById('book-form').addEventListener('submit', function (e) {
-//     const title1 = document.getElementById('title1');
-//     const title2 = document.getElementById('title2');
-//     const title3 = document.getElementById('title3');
-//     const title4 = document.getElementById('title4');
-//     const title5 = document.getElementById('title5');
-//     const title6 = document.getElementById('title6');
-//     const title7 = document.getElementById('title7');
+//             const title1 = document.getElementById('title1');
+//             const title2 = document.getElementById('title2');
+//             const title3 = document.getElementById('title3');
+//             const title4 = document.getElementById('title4');
+//             const title5 = document.getElementById('title5');
+//             const title6 = document.getElementById('title6');
+//             const title7 = document.getElementById('title7');
 
-        let title1 = title1 / 100;
-    let title3 = title1 / (title2 * title2);
-    let title4 = 18.5 * (title2 * title2);
-    let title5 = 24 * (title2 * title2);
-    let title6 = (title2 - title4).toFixed(1);
-    let title7 = (title5 - title2).toFixed(1);
+let title1 = title1 / 100;
+let title3 = title1 / (title2 * title2);
+let title4 = 18.5 * (title2 * title2);
+let title5 = 24 * (title2 * title2);
+let title6 = (title2 - title4).toFixed(1);
+let title7 = (title5 - title2).toFixed(1);
 
-    if (title1 === 0 || title2 === 0) {
-        row.innerHTML = `
+if (title1 === 0 || title2 === 0) {
+    row.innerHTML = `
             <td>打字阿~</td>
     `;
-    }
-    else if (18.5 > title3) {
-        row.innerHTML = `
+} else if (18.5 > title3) {
+    row.innerHTML = `
             <td>${title2} 體重過輕</td>
             <td>${title3}</td>
             <td>應該增加${title6}公斤</td>
     `;
-    }
-    else if (18.5 <= title3 && title3 < 24) {
-        row.innerHTML = `
+} else if (18.5 <= title3 && title3 < 24) {
+    row.innerHTML = `
             <td>${title2} 正常範圍</td>
             <td>${title3}</td>
             <td>繼續保持</td>
     `;
-    }
-    else if (24 <= title3 && title3 < 27) {
-        row.innerHTML = `
+} else if (24 <= title3 && title3 < 27) {
+    row.innerHTML = `
             <td>${title2} 體重稍重</td>
             <td>${title3}</td>
             <td>應該增加${title7}公斤</td>
     `;
-    }
-    else if (27 <= title3 && title3 < 30) {
-        row.innerHTML = `
+} else if (27 <= title3 && title3 < 30) {
+    row.innerHTML = `
             <td>${title2} 輕度肥胖</td>
             <td>${title3}</td>
             <td>應該增加${title7}公斤</td>
     `;
-    }
-    else if (30 <= title3 && title3 < 35) {
-        row.innerHTML = `
+} else if (30 <= title3 && title3 < 35) {
+    row.innerHTML = `
             <td>${title2} 中度肥胖</td>
             <td>${title3}</td>
             <td>應該增加${title7}公斤</td>
     `;
-    }
-    else if (35 <= title3) {
-        row.innerHTML = `
+} else if (35 <= title3) {
+    row.innerHTML = `
             <td>${title2} 重度肥</td>
             <td>${title3}</td>
             <td>應該增加${title7}公斤</td>
     `;
-    }
-    e.preventDefault();
-
+}
+e.preventDefault();
